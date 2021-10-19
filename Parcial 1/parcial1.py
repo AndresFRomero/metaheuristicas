bf130ad8b04a1969fe0756c00590915824c26323
AndresRomero <fiorenttos227@gmail.com>
2021-10-19 15:11:45 -0500

    Parcial Completed

--- Parcial 1/parcial1.py
+++ Parcial 1/parcial1.py
@@ -36,10 +36,11 @@ def objFun(group:set, dM: dict):
             FO += dM[i,j]
     return FO/len(group) # mean FO 
 
-def formalElimination(group:set, dM: dict):
+def formalElimination(group:set, dM: dict, strong:bool):
     # one single elimination.
     iniFO = 0
     iniGroup = group
+    iniEl = '-1'
 
     for i in group:
         newGroup = group.difference(set([i]))
@@ -47,12 +48,42 @@ def formalElimination(group:set, dM: dict):
         if(newFO > iniFO):
             iniGroup = newGroup
             iniFO = newFO
-    return iniGroup
+            iniEl = i
+
+    if strong:
+        return iniGroup
+    else:
+        return iniEl
 
 def randomElimination(group:set):
     # one single elimination
     group.remove(random.sample(group,1)[0])
 
+def localSearch(students:dict, group:set, dM:dict, ObjFun: float):
+    # Inits
+    improve = True
+    improveFO = ObjFun
+    improveGroup = group
+
+    count = 0
+    while improve:
+        removeCandidate = formalElimination(improveGroup, dM, False)
+        candidateGroup = improveGroup # Init
+        improve = False # exit while
+
+        for i in set(students.keys()).difference(improveGroup):
+            test = improveGroup.difference(set([removeCandidate]))
+            newFO = objFun(test, dM)
+            if newFO > improveFO:
+                improveFO = newFO
+                candidateGroup = test
+                improve = True
+
+        improveGroup = candidateGroup
+    count += 1
+    return improveFO, improveGroup
+
+# Just the pseudo-randomized heuristic
 def constructive(students:dict, dM:dict, n:int, alpha: float):
     group = set(students.keys())
 
@@ -60,11 +91,24 @@ def constructive(students:dict, dM:dict, n:int, alpha: float):
         if random.random() < alpha:
             randomElimination(group)
         else:
-            group = formalElimination(group, dM)
+            group = formalElimination(group, dM, True)
 
     return objFun(group, dM), group 
 
-# Just the pseudo-randomized heuristic
 print('Pseudo-Random-Heuristic', constructive(students, dM, 5, 0.2)) 
 
 # Metaheuristic implementation
+def GRASP(students:dict, dM:dict, n:int, alpha: float, maxIters: int):
+
+    bestSol = (0,set())
+
+    for _ in range(maxIters):
+        objFun, solution = constructive(students, dM, n, alpha)
+        objFun, solution = localSearch(students, solution, dM, objFun)
+
+        if objFun > bestSol[0]:
+            bestSol = (objFun, solution)
+
+    return bestSol
+
+print('GRASP Procedure', GRASP(students, dM, 5, 0.2, 200))
