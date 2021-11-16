7d61bd65a99e9a8c4a20092e62b862935d9a97f0
AndresRomero <fiorenttos227@gmail.com>
2021-11-16 00:27:05 -0500

    first commit

---
+++ 5. Genetico 2DKnapsack/brkga.py
@@ -0,0 +1,8 @@
+# BRKGA: Biased Random Key Genertic Algotirhm for 2D Knapsack
+# Andres F. Romero
+
+# Libraries
+import random
+
+class BRKGA_Knapsack:
+    pass
---
+++ 5. Genetico 2DKnapsack/brkgaValidator.py
@@ -0,0 +1,30 @@
+# BRKGA: Biased Random Key Genertic Algotirhm for 2D Knapsack
+
+import brkga
+import os
+import time
+
+def loadData(file : str):
+    with open(file, 'r') as fp:
+        problem = { 'timeMatrix' : {} }
+        n = 1
+        for line in fp:
+            line = line.rstrip('\n')
+            if n >= 4:
+                times = line.replace("  ", " ").split(" ")
+                for idx, time in enumerate(times):
+                    problem['timeMatrix'][n-3,idx+1] = int(time)
+            n += 1
+    fp.close()
+    problem['machines'] = [ i+1 for i in range(5)]
+    problem['jobs'] = set([ i+1 for i in range(20)])
+
+    return problem
+
+ACO = brkga.AntColonyOptimization()
+
+file = './Tests/tai20_5_010.txt'
+problem = loadData(file)
+start_time = time.time()
+route, makespan, deadtime = ACO.main(problem)
+print(file, makespan, round(time.time()-start_time,2) )
